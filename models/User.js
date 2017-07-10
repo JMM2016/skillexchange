1295d82d9282761e787032d1b4809228b1737d91
Marc Deheeger <mcdeheeger@Marcs-MacBook-Pro.local>
2017-07-10 17:37:59 -0500

    updating user model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+// User model
+// ==============
+
+// Require mongoose
+var mongoose = require("mongoose");
+
+// Create a schema class using mongoose's schema method
+var Schema = mongoose.Schema;
+
+// Create the headlineSchema with our schema class
+var userSchema = new Schema({
+    // firstName, a string, must be entered
+    firstName: {
+        type: String,
+        required: true,
+    },
+    // lastName, a string, must be entered
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        min: 18,
+        max: 65 },
+    password: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    interests: {
+        type: [String]
+    },
+    needs: {
+        type: [String]
+    },
+    updated: { type: Date, default: Date.now }
+});
+
+// Create the User model using the userSchema
+var User = mongoose.model("User", userSchema);
+
+// Export the User model
+module.exports = User;
