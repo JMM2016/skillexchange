a3805dc412c4f53ecee9c1994ab1a9081e2328ba
Marc Deheeger <mcdeheeger@dhcp-10-102-148-1.wireless.northwestern.private>
2017-07-15 11:29:01 -0500

    updating file structure

---
+++ config/userRoutes.js
@@ -0,0 +1,95 @@
+// Server routes
+// =============
+
+// Bring in the Headline and Note mongoose models
+// var User = require("../models/User");
+
+
+module.exports = function (router) {
+
+    // middleware to use for all requests
+    router.use(function (req, res, next) {
+        // do logging
+        console.log('Something is happening.');
+        next(); // make sure we go to the next routes and don't stop here
+    });
+
+    // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+    router.get("/", function (req, res) {
+        res.render("index");
+    });
+
+    // Middleware to use for all requests
+    router.use(function (req, res, next) {
+        // do logging
+        console.log('Something is happening.');
+        next(); // make sure we go to the next routes and don't stop here
+    });
+
+
+
+    // USER ROUTES
+    router.route('/user')
+
+    // Create a new user...
+        .post(function (req, res) {
+            var user = new User();
+            user.firstName = req.body.firstname;
+            user.skills = req.body.skills;
+            user.needs = req.body.needs;
+            // ... and save new user...
+            user.save(function (err) {
+                if (err)
+                    res.send(err);
+                res.json({message: 'User created!'});
+            });
+        })
+
+        // Find all users...
+        .get(function (req, res) {
+            User.find(function (err, users) {
+                if (err)
+                    res.send(err);
+                res.json(users);
+            });
+        })
+
+    router.route('/user/:user_id')
+
+    // Find specific user
+        .get(function (req, res) {
+            User.findById(req.params.user_id, function (err, user) {
+                if (err)
+                    res.send(err);
+                console.log(err);
+                res.json(user);
+                console.log(user);
+            });
+        });
+
+    // NEED SEARCH ROUTE
+    router.route('/user/needs/:user_need')
+
+        .get(function (req, res) {
+            User.find({needs: req.user_need}, function (err, user) {
+                if (err)
+                    res.send(err);
+                console.log(err);
+                res.json(user);
+                console.log(user);
+            });
+        });
+
+    // SKILL SEARCH ROUTE
+    router.route('/user/skills/:user_skill')
+
+        .get(function (req, res) {
+            User.find({skills: req.params.user_skill}, function (err, user) {
+                if (err)
+                    res.send(err);
+                console.log(err);
+                res.json(user);
+                console.log(user);
+            });
+        });
+};
--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 var express = require("express");
 // Instantiate our Express App
 var app = express();
-
 var mongoose = require("mongoose");
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var db = process.env.MONGODB_URI || "mongodb://localhost/skillexchange";
-
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -17,28 +19,23 @@ app.use(bodyParser.json());
 // Designate our public folder as a static directory
 app.use(express.static(__dirname + "/public"));
 
+var app = express();
 // Set up our port to be either the host's designated port, or 3000
 var PORT = process.env.PORT || 3000;
 
 // Set up an Express Router
 var router = express.Router();
 
+app.use(router)
 // Require our routes files and pass our router object
-require("./app/config/userRoutes")(router);
-
-// middleware to use for all requests
-router.use(function (req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
+require("./config/userRoutes")(router);
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function (req, res) {
-    res.send('./public/index');
+    res.send('index');
 });
 
-app.use('/api', router);
+// app.use('/api', router);
 
 // Connect mongoose to our database
 mongoose.connect(db, function (error) {
