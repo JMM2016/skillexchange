7590ab94357385b2d1955ca8be5155aa8f779f8f
JPSauSara <jpsausara@users.noreply.github.com>
2017-07-23 19:39:30 -0500

    fixed css style reference

--- app/pages/home.js
+++ app/pages/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import NavLink from './NavLink';
 var Link = require("react-router").Link;
+// import syles from './style.css';
 
 
 
@@ -13,7 +14,6 @@ export default class Home extends React.Component {
 				<h1 className="jumbotron">SkillShare</h1>
 				<div className="row">
 					<div className="col-md-10">
-						<h1 className= "mainScreen"> PIC GOES HERE</h1>
 						<img src="./img/friend.png" />
 					</div>
 				</div>
--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,9 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: 'style-loader!css-loader'
+                // loader: 'style-loader!css-loader'
+                loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]', 
+
             },
 
             {
